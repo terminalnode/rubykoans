775441f8b1e39ab87a4c932aa9570d5d334c7fec
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-01-12 15:58:39 +0100

    223/282 steps

--- about_inheritance.rb
+++ about_inheritance.rb
@@ -24,23 +24,23 @@ class AboutInheritance < Neo::Koan
   end
 
   def test_subclasses_have_the_parent_as_an_ancestor
-    assert_equal __, Chihuahua.ancestors.include?(Dog)
+    assert_equal true, Chihuahua.ancestors.include?(Dog)
   end
 
   def test_all_classes_ultimately_inherit_from_object
-    assert_equal __, Chihuahua.ancestors.include?(Object)
+    assert_equal true, Chihuahua.ancestors.include?(Object)
   end
 
   def test_subclasses_inherit_behavior_from_parent_class
     chico = Chihuahua.new("Chico")
-    assert_equal __, chico.name
+    assert_equal "Chico", chico.name
   end
 
   def test_subclasses_add_new_behavior
     chico = Chihuahua.new("Chico")
-    assert_equal __, chico.wag
+    assert_equal :happy, chico.wag
 
-    assert_raise(___) do
+    assert_raise(NoMethodError) do
       fido = Dog.new("Fido")
       fido.wag
     end
@@ -48,36 +48,39 @@ class AboutInheritance < Neo::Koan
 
   def test_subclasses_can_modify_existing_behavior
     chico = Chihuahua.new("Chico")
-    assert_equal __, chico.bark
+    assert_equal "yip", chico.bark
 
     fido = Dog.new("Fido")
-    assert_equal __, fido.bark
+    assert_equal "WOOF", fido.bark
   end
 
   # ------------------------------------------------------------------
 
   class BullDog < Dog
     def bark
+      # that's cool. so super is refering to the output of the same
+      # method in the super class. not the super class proper
       super + ", GROWL"
     end
   end
 
   def test_subclasses_can_invoke_parent_behavior_via_super
     ralph = BullDog.new("Ralph")
-    assert_equal __, ralph.bark
+    assert_equal "WOOF, GROWL", ralph.bark
   end
 
   # ------------------------------------------------------------------
 
   class GreatDane < Dog
     def growl
+      # and in fact can't refer to the super class proper? less cool
       super.bark + ", GROWL"
     end
   end
 
   def test_super_does_not_work_cross_method
     george = GreatDane.new("George")
-    assert_raise(___) do
+    assert_raise(NoMethodError) do
       george.growl
     end
   end

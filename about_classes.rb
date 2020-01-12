9bada183ba9f91d573fa661773deb72e41efa570
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-01-12 15:40:51 +0100

    208/282 steps

--- about_classes.rb
+++ about_classes.rb
@@ -6,36 +6,37 @@ class AboutClasses < Neo::Koan
 
   def test_instances_of_classes_can_be_created_with_new
     fido = Dog.new
-    assert_equal __, fido.class
+    assert_equal Dog, fido.class
   end
 
   # ------------------------------------------------------------------
 
   class Dog2
     def set_name(a_name)
-      @name = a_name
+      @name = a_name # @ is for instance variables
     end
   end
 
   def test_instance_variables_can_be_set_by_assigning_to_them
     fido = Dog2.new
-    assert_equal __, fido.instance_variables
+    assert_equal [], fido.instance_variables
 
     fido.set_name("Fido")
-    assert_equal __, fido.instance_variables
+    assert_equal [:@name], fido.instance_variables
   end
 
   def test_instance_variables_cannot_be_accessed_outside_the_class
     fido = Dog2.new
     fido.set_name("Fido")
 
-    assert_raise(___) do
+    assert_raise(NoMethodError) do
       fido.name
     end
 
-    assert_raise(___) do
+    assert_raise(SyntaxError) do
       eval "fido.@name"
       # NOTE: Using eval because the above line is a syntax error.
+      # And in 2.7 it's still a syntax error
     end
   end
 
@@ -43,15 +44,21 @@ class AboutClasses < Neo::Koan
     fido = Dog2.new
     fido.set_name("Fido")
 
-    assert_equal __, fido.instance_variable_get("@name")
+    assert_equal "Fido", fido.instance_variable_get("@name")
+
+    # Strange. And here's what happens if the variable doesn't exist
+    assert_equal nil, fido.instance_variable_get("@lolol")
   end
 
   def test_you_can_rip_the_value_out_using_instance_eval
     fido = Dog2.new
     fido.set_name("Fido")
 
-    assert_equal __, fido.instance_eval("@name")  # string version
-    assert_equal __, fido.instance_eval { @name } # block version
+    assert_equal "Fido", fido.instance_eval("@name")  # string version
+    assert_equal "Fido", fido.instance_eval { @name } # block version
+
+    # And here's what happens if the instance doesn't exist
+    assert_equal nil, fido.instance_eval { @lolol }
   end
 
   # ------------------------------------------------------------------
@@ -69,7 +76,8 @@ class AboutClasses < Neo::Koan
     fido = Dog3.new
     fido.set_name("Fido")
 
-    assert_equal __, fido.name
+    # OK so like a getter method
+    assert_equal "Fido", fido.name
   end
 
   # ------------------------------------------------------------------
@@ -87,7 +95,8 @@ class AboutClasses < Neo::Koan
     fido = Dog4.new
     fido.set_name("Fido")
 
-    assert_equal __, fido.name
+    # Very neat.
+    assert_equal "Fido", fido.name
   end
 
   # ------------------------------------------------------------------
@@ -101,7 +110,8 @@ class AboutClasses < Neo::Koan
     fido = Dog5.new
 
     fido.name = "Fido"
-    assert_equal __, fido.name
+    # Super neat.
+    assert_equal "Fido", fido.name
   end
 
   # ------------------------------------------------------------------
@@ -115,22 +125,26 @@ class AboutClasses < Neo::Koan
 
   def test_initialize_provides_initial_values_for_instance_variables
     fido = Dog6.new("Fido")
-    assert_equal __, fido.name
+    # Right, so the constructor is always called initialize. Neat
+    assert_equal "Fido", fido.name
   end
 
   def test_args_to_new_must_match_initialize
-    assert_raise(___) do
+    assert_raise(ArgumentError) do
       Dog6.new
     end
     # THINK ABOUT IT:
     # Why is this so?
+    #
+    # Because initialize was called with no arguments, doh-doyh
   end
 
   def test_different_objects_have_different_instance_variables
     fido = Dog6.new("Fido")
     rover = Dog6.new("Rover")
 
-    assert_equal __, rover.name != fido.name
+    # Mind != blown
+    assert_equal true, rover.name != fido.name
   end
 
   # ------------------------------------------------------------------
@@ -159,32 +173,33 @@ class AboutClasses < Neo::Koan
     fido = Dog7.new("Fido")
 
     fidos_self = fido.get_self
-    assert_equal __, fidos_self
+    assert_equal fido, fidos_self
   end
 
   def test_to_s_provides_a_string_version_of_the_object
     fido = Dog7.new("Fido")
-    assert_equal __, fido.to_s
+    assert_equal fido.name, fido.to_s
   end
 
   def test_to_s_is_used_in_string_interpolation
     fido = Dog7.new("Fido")
-    assert_equal __, "My dog is #{fido}"
+    assert_equal "My dog is Fido", "My dog is #{fido}"
   end
 
   def test_inspect_provides_a_more_complete_string_version
     fido = Dog7.new("Fido")
-    assert_equal __, fido.inspect
+    # basically like __repr__ in python
+    assert_equal "<Dog named 'Fido'>", fido.inspect
   end
 
   def test_all_objects_support_to_s_and_inspect
     array = [1,2,3]
 
-    assert_equal __, array.to_s
-    assert_equal __, array.inspect
+    assert_equal "[1, 2, 3]", array.to_s
+    assert_equal "[1, 2, 3]", array.inspect
 
-    assert_equal __, "STRING".to_s
-    assert_equal __, "STRING".inspect
+    assert_equal "STRING", "STRING".to_s
+    assert_equal "\"STRING\"", "STRING".inspect
   end
 
 end
